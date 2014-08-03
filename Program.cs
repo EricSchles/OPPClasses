85e4a371246ea24dd284c9f85a6bbc40bf247fca
Eric Schles <ericschles@gmail.com>
2014-08-03 12:58:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPClasses
+{
+    class Program
+    {
+
+        class Car
+        {
+            private string color;
+
+            public Car()
+            {
+                this.color = "Blue";
+            }
+
+            public Car(string color)
+            {
+                this.color = color;
+            }
+
+            public string Describe()
+            {
+                return "This car is " + color;
+            }
+
+            public string Color
+            {
+                get { return color; }
+                set { color = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Car car;
+            car = new Car();
+            Console.WriteLine(car.Describe());
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("Green");
+            Console.WriteLine(car.Describe());
+
+            Console.ReadKey();
+
+        }
+    }
+}
